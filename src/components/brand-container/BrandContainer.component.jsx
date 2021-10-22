01c60b7f0969517caa882c029d5738939536a78d
Amith Sai <amithsai78@gmail.com>
2021-10-23 00:56:08 +0530

    Updated Brand Component

--- src/components/brand-container/BrandContainer.component.jsx
+++ src/components/brand-container/BrandContainer.component.jsx
@@ -6,7 +6,7 @@ function BrandContainer() {
     return (
         <section className="brands">
             <div className="container d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center justify-content-between"n>
                     {brands.map(({ id, name, image }) => (
                         <a href="/" key={id}>
                             <img src={image} alt={name} />
